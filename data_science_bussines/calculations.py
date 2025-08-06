e844c91f1a5b9c5427cba47355b339f682307c3d
tomaslond12-jpg <tomaslond.12@gmail.com>
2025-08-06 01:46:57 +0000

    Co-authored-by: Camilo Orozco <CamiloOrozco-dev@users.noreply.github.com>

---
+++ data_science_bussines/calculations.py
@@ -0,0 +1,36 @@
+import pandas
+
+class Calculations:
+    def __init__(self):
+        self._file_path : str = "data/data.csv" 
+    
+    def load_data(self):        
+        df = pandas.read_csv(self._file_path)
+        return df
+
+
+    def get_public_orgs(self):
+        df = self.load_data()
+        public_df = df[df["Public?"] == 1]
+        num = len(public_df)
+        print("There are ", num, "public organizations.")
+        return num
+
+
+    def revenue_per_industry(self):
+        df = self.load_data()
+        revenue_by_industry = df.groupby("Industry")["Revenue"].sum()
+        count_by_industry = df["Industry"].value_counts()
+
+        rev_ratio = revenue_by_industry / count_by_industry
+
+        return rev_ratio
+
+    def highest_revenue_industry(self):
+        df = self.load_data()
+        revenue_by_industry = df.groupby("Industry")["Revenue"].sum()
+        top_revenue_industry = revenue_by_industry.idxmax()
+
+        return top_revenue_industry
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+
+from .data_science_bussines.calculations import Calculations
+
+def main():
+    calculations = Calculations()
+    
+    num_public_orgs = calculations.get_public_orgs()
+    rev_per_industry = calculations.revenue_per_industry()
+    highest_rev_industry = calculations.highest_revenue_industry()
+
+    print("Number of public organizations:", num_public_orgs)
+    print("Revenue per industry:")
+    print(rev_per_industry)
+    print("Industry with the highest revenue:", highest_rev_industry)
+
+    # Siempre que quiero utilizar una clase, debeo hacer una instancia para operar el objeto  'calculations = Calculations()'
+
+if __name__ == "__main__":
+    main()
